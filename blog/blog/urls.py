6ddeb70a621d704bd7a79c21eb71db372c2a71b6
joy-python1 <joypython@yandex.ru>
2021-10-02 19:23:44 +0300

    lab4

---
+++ blog/blog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from articles import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('articles/', views.archive, name="archive"),
+    path(r'^article/(?P<article_id>\d+)$', views.get_article, name='get_article')
+]
